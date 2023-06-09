3d7c8bc03c6e3d6d056df0a6d8618f83b9692de4
MYO <MYO@d.c>
2023-06-09 18:44:41 +0300

    add sender_mail work only in develop, need server

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ekaterina/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ekaterina/IdeaProjects/flutter/hr"
+export "FLUTTER_APPLICATION_PATH=/Users/ekaterina/IdeaProjects/flutter/hr_test"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- lib/service/mail_send_service.dart
+++ lib/service/mail_send_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:file_picker/file_picker.dart';
 
 class MailSendService {
@@ -9,5 +11,61 @@ class MailSendService {
     this.name = name;
     this.number = number;
     this.file = file;
+    sendEmail('hr.library.offer@gmail.com', name, number, file: this.file);
+  }
+
+  Future<void> sendEmail(
+    String recipient,
+    String name,
+    String number, {
+    PlatformFile file,
+  }) async {
+    final apiKey =
+        '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';
+    final url = Uri.parse('https://api.sendgrid.com/v3/mail/send');
+
+    final headers = {
+      'Authorization': 'Bearer $apiKey',
+      'Content-Type': 'application/json',
+    };
+
+    final body = {
+      'personalizations': [
+        {
+          'to': [
+            {'email': recipient}
+          ]
+        }
+      ],
+      'from': {'email': 'hr.library.offer@gmail.com'},
+      'subject': 'OFFER',
+      'content': [
+        {'type': 'text/plain', 'value': 'name: $name\nnumber: $number'}
+      ]
+    };
+
+    if (file != null) {
+      final fileBytes = await file.bytes;
+      final base64File = base64Encode(fileBytes);
+      body['attachments'] = [
+        {
+          'content': base64File,
+          'filename': file.name,
+        }
+      ];
+    }
+
+    final response = await http.post(
+      url,
+      headers: headers,
+      body: jsonEncode(body),
+    );
+
+    if (response.statusCode == 202) {
+      print('Email sent successfully!');
+    } else {
+      print('Failed to send email. Status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+    }
   }
 }
